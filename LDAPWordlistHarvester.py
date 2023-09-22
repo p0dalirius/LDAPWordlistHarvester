ceb2dbb3d9cf53422916e2c5258dfc7707b82d8b
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2023-09-22 22:12:25 +0200

    Public release

---
+++ LDAPWordlistHarvester.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : LDAPWordlistHarvester.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 22 Sep 2023
+
+
+import argparse
+from sectools.windows.ldap import raw_ldap_query, init_ldap_session
+from sectools.windows.crypto import nt_hash, parse_lm_nt_hashes
+import os
+import sys
+import sqlite3
+import json
+import xlsxwriter
+import re
+
+
+VERSION = "1.1"
+
+
+def get_domain_from_distinguished_name(distinguishedName):
+    domain = None
+    if "dc=" in distinguishedName.lower():
+        distinguishedName = distinguishedName.lower().split(',')[::-1]
+
+        while distinguishedName[0].startswith("dc="):
+            if domain is None:
+                domain = distinguishedName[0].split('=',1)[1]
+            else:
+                domain = distinguishedName[0].split('=', 1)[1] + "." + domain
+            distinguishedName = distinguishedName[1:]
+
+    return domain
+
+
+def get_ou_path_from_distinguished_name(distinguishedName):
+    ou_path = None
+    if "ou=" in distinguishedName.lower():
+        distinguishedName = distinguishedName.lower().split(',')[::-1]
+
+        # Skip domain
+        while distinguishedName[0].startswith("dc="):
+            distinguishedName = distinguishedName[1:]
+
+        while distinguishedName[0].startswith("ou="):
+            if ou_path is None:
+                ou_path = distinguishedName[0].split('=',1)[1]
+            else:
+                ou_path = ou_path + " --> " + distinguishedName[0].split('=',1)[1]
+            distinguishedName = distinguishedName[1:]
+
+        return ou_path
+    else:
+        return ou_path
+
+
+def parseArgs():
+    print("LDAPWordlistHarvester.py v%s - by @podalirius_\n" % VERSION)
+
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Verbose mode. (default: False)")
+    parser.add_argument("-o", "--outputfile", default="wordlist.txt", help="Path to output file of wordlist.")
+
+    authconn = parser.add_argument_group('Authentication & connection')
+    authconn.add_argument("--dc-ip", required=True, action="store", metavar="ip address", help="IP Address of the domain controller or KDC (Key Distribution Center) for Kerberos. If omitted it will use the domain part (FQDN) specified in the identity parameter")
+    authconn.add_argument("-d", "--domain", dest="auth_domain", metavar="DOMAIN", action="store", default="", help="(FQDN) domain to authenticate to")
+    authconn.add_argument("-u", "--user", dest="auth_username", metavar="USER", action="store", default="", help="user to authenticate with")
+
+    secret = parser.add_argument_group("Credentials")
+    cred = secret.add_mutually_exclusive_group()
+    cred.add_argument("--no-pass", default=False, action="store_true", help="Don't ask for password (useful for -k)")
+    cred.add_argument("-p", "--password", dest="auth_password", metavar="PASSWORD", action="store", default=None, help="Password to authenticate with")
+    cred.add_argument("-H", "--hashes", dest="auth_hashes", action="store", metavar="[LMHASH:]NTHASH", help="NT/LM hashes, format is LMhash:NThash")
+    cred.add_argument("--aes-key", dest="auth_key", action="store", metavar="hex key", help="AES key to use for Kerberos Authentication (128 or 256 bits)")
+    secret.add_argument("-k", "--kerberos", dest="use_kerberos", action="store_true", help="Use Kerberos authentication. Grabs credentials from .ccache file (KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use the ones specified in the command line")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    options = parseArgs()
+    if options.auth_hashes is not None:
+        if ":" not in options.auth_hashes:
+            options.auth_hashes = ":" + options.auth_hashes
+    auth_lm_hash, auth_nt_hash = parse_lm_nt_hashes(options.auth_hashes)
+
+    wordlist = []
+
+    print("[>] Getting information from remote LDAP host ... ", end="", flush=True)
+    ldap_server, ldap_session = init_ldap_session(auth_domain=options.auth_domain, auth_username=options.auth_username, auth_password=options.auth_password, auth_lm_hash=auth_lm_hash, auth_nt_hash=auth_nt_hash, auth_dc_ip=options.dc_ip)
+    configurationNamingContext = ldap_server.info.other["configurationNamingContext"]
+    defaultNamingContext = ldap_server.info.other["defaultNamingContext"]
+    print("done.")
+
+
+    # Extracting AD sites
+    print("[>] Extracting AD Sites from LDAP ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=configurationNamingContext,
+        query="(objectClass=site)", attributes=["name", "description"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        added_words += ' '.join(data["description"]).split(' ')
+        added_words += data["name"].split(' ')
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Extracting user and computer
+    print("[>] Extracting user and computer names from LDAP ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=defaultNamingContext,
+        query="(|(objectClass=person)(objectClass=user)(objectClass=computer))", attributes=["name", "sAMAccountName"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        if len(data["sAMAccountName"]) != 0:
+            if type(data["sAMAccountName"]) == list:
+                added_words += ' '.join([e for e in data["sAMAccountName"] if len(e) != 0]).split(' ')
+            else:
+                added_words.append(data["sAMAccountName"])
+        if len(data["sAMAccountName"]) != 0:
+            if type(data["sAMAccountName"]) == list:
+                added_words += ' '.join([e for e in data["sAMAccountName"] if len(e) != 0]).split(' ')
+            else:
+                added_words.append(data["sAMAccountName"])
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Extracting descriptions
+    print("[>] Extracting descriptions of all LDAP objects ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=defaultNamingContext,
+        query="(description=*)", attributes=["description"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        added_words += ' '.join(data["description"]).split(' ')
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Extracting group names
+    print("[>] Extracting group names of all LDAP objects ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=defaultNamingContext,
+        query="(objectCategory=group)", attributes=["name"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        added_words.append(data["name"])
+        added_words += ' '.join(data["name"]).split(' ')
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Extracting organizationalUnit
+    print("[>] Extracting organizationalUnit of all LDAP objects ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=defaultNamingContext,
+        query="(objectCategory=organizationalUnit)", attributes=["name"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        added_words.append(data["name"])
+        added_words += ' '.join(data["name"]).split(' ')
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Extracting servicePrincipalName
+    print("[>] Extracting servicePrincipalName of all LDAP objects ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=defaultNamingContext,
+        query="(servicePrincipalName=*)", attributes=["servicePrincipalName"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        for spn in data["servicePrincipalName"]:
+            added_words.append(spn)
+            added_words += spn.split('/')
+            added_words += spn.replace('.','/').split('/')
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Extracting trustedDomains
+    print("[>] Extracting trustedDomains from LDAP ... ", end="", flush=True)
+    ldap_results = raw_ldap_query(
+        auth_domain=options.auth_domain, auth_dc_ip=options.dc_ip, auth_username=options.auth_username, auth_password=options.auth_password, auth_hashes=options.auth_hashes,
+        searchbase=defaultNamingContext,
+        query="(objectClass=trustedDomain)", attributes=["name"]
+    )
+    added_words = []
+    for dn, data in ldap_results.items():
+        added_words.append(data["name"])
+        added_words += data["name"].split('.')
+    added_words = list(set(added_words))
+    print("found %d words" % (len(added_words)), flush=True)
+    len_before = len(wordlist)
+    wordlist = list(set(wordlist + added_words))
+    len_after = len(wordlist)
+    print(" └──[+] Added %d unique words to wordlist." % (len_after - len_before))
+
+
+    # Exporting output
+    print()
+    print("[+] Writing %d words to '%s' ... " % (len(wordlist), options.outputfile))
+    basepath = os.path.dirname(options.outputfile)
+    filename = os.path.basename(options.outputfile)
+    if basepath not in [".", ""]:
+        if not os.path.exists(basepath):
+            os.makedirs(basepath)
+        path_to_file = basepath + os.path.sep + filename
+    else:
+        path_to_file = filename
+    f = open(path_to_file, "w")
+    for word in wordlist:
+        f.write(word+"\n")
+    f.close()
+    print("[+] Bye Bye!")
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# LDAPWordlistHarvester
+![](.github/banner.png)
 
+<p align="center">
+    A system administration or post-exploitation script to automatically extract the bitlocker recovery keys from a domain.
+    <br>
+    <img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/p0dalirius/ExtractBitlockerKeys">
+    <a href="https://twitter.com/intent/follow?screen_name=podalirius_" title="Follow"><img src="https://img.shields.io/twitter/follow/podalirius_?label=Podalirius&style=social"></a>
+    <a href="https://www.youtube.com/c/Podalirius_?sub_confirmation=1" title="Subscribe"><img alt="YouTube Channel Subscribers" src="https://img.shields.io/youtube/channel/subscribers/UCF_x5O7CSfr82AfNVTKOv_A?style=social"></a>
+    <br>
+</p>
+
+## Features
+
+ - [x] Creates a wordlist based on the following information found in the LDAP:
+   - [x] **User**: `name` and `sAMAccountName`
+   - [x] **Computer**: `name` and `sAMAccountName`
+   - [x] **Groups**: `name`
+   - [x] **Organizational Units**: `name`
+   - [x] **Active Directory Sites**: `name` and `descriptions`
+   - [x] **All LDAP objects**: `descriptions`
+ - [x] Choose wordlist output file name with option `--outputfile`
+
+## Demonstration
+
+To generate a wordlist from the LDAP of the domain `domain.local` you can use this command:
+
+```
+./LDAPWordlistHarvester.py -d 'domain.local' -u 'Administrator' -p 'P@ssw0rd123!' --dc-ip 192.168.1.101
+```
+
+You will get the following output:
+
+![](./.github/example.png)
+
+
+## Usage
+
+```
+$ ./LDAPWordlistHarvester.py -h
+LDAPWordlistHarvester.py v1.1 - by @podalirius_
+
+usage: LDAPWordlistHarvester.py [-h] [-v] [-o OUTPUTFILE] --dc-ip ip address [-d DOMAIN] [-u USER] [--no-pass | -p PASSWORD | -H [LMHASH:]NTHASH | --aes-key hex key] [-k]
+
+options:
+  -h, --help            show this help message and exit
+  -v, --verbose         Verbose mode. (default: False)
+  -o OUTPUTFILE, --outputfile OUTPUTFILE
+                        Path to output file of wordlist.
+
+Authentication & connection:
+  --dc-ip ip address    IP Address of the domain controller or KDC (Key Distribution Center) for Kerberos. If omitted it will use the domain part (FQDN) specified in the identity parameter
+  -d DOMAIN, --domain DOMAIN
+                        (FQDN) domain to authenticate to
+  -u USER, --user USER  user to authenticate with
+
+Credentials:
+  --no-pass             Don't ask for password (useful for -k)
+  -p PASSWORD, --password PASSWORD
+                        Password to authenticate with
+  -H [LMHASH:]NTHASH, --hashes [LMHASH:]NTHASH
+                        NT/LM hashes, format is LMhash:NThash
+  --aes-key hex key     AES key to use for Kerberos Authentication (128 or 256 bits)
+  -k, --kerberos        Use Kerberos authentication. Grabs credentials from .ccache file (KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use the ones specified in the command line
+```
